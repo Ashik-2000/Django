9b50d516b12368a693e80960fd80bca122eb7c19
Ashikul Islam <ashikulislamfb@gmail.com>
2025-02-10 23:37:45 +0600

    modified

--- FormWithAuth/ForPractice/MyApp/views.py
+++ FormWithAuth/ForPractice/MyApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from .models import Student
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 # Authetication Functions
